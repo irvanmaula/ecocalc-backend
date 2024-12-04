23971c37c71ec4810a8e1901573667c271c55fa7
irvanmaula <irvanmaul4321@gmail.com>
2024-12-04 20:14:46 +0700

    uodate repo

--- login/backend/app.js
+++ login/backend/app.js
@@ -22,6 +22,6 @@ const db = admin.firestore();
 app.use('/', authRoutes);
 
 
-const hostname = '0.0.0.0';
+const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,HOST, () => console.log(`Server running on http://${HOST}:${PORT}`));
